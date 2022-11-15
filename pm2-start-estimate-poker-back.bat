b8ed645046d230792be93b14e720d86ab466e76a
Ali Akbar Shahi <ali.akbar.shahi.junior@gmail.com>
2022-11-15 17:55:17 +0330

    add startup scripts

---
+++ pm2-start-estimate-poker-back.bat
@@ -0,0 +1,3 @@
+pm2 start D:\Front\estimate-poker\backend\index.js --name estimate-poker-back
+pm2 start D:\Front\estimate-poker\frontend\dist\angular-estimate-poker\server.js --name estimate-poker-front
+pause
---
+++ pm2-start-estimate-poker-front.bat
@@ -0,0 +1,2 @@
+pm2 start D:\Front\estimate-poker\frontend\dist\angular-estimate-poker\server.js --name estimate-poker-front
+pause
